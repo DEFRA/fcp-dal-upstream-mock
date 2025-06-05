3ff73f3235c515c77337cb011a3e6775b09be9b0
Robin Knipe <robin.knipe@defra.gov.uk>
2025-06-05 14:18:27 +0100

    feat: add routes for fake people

---
+++ src/routes/v2/person.js
@@ -0,0 +1,43 @@
+import Boom from '@hapi/boom'
+import { crnIdCache } from '../../../fixtures/relations/crn-person-id.js'
+import { createPerson } from '../../factories/person.factory.js'
+import { pagination, pagination0 } from '../../plugins/data/pagination.js'
+
+const idCrnCache = Object.fromEntries(Object.entries(crnIdCache).map(([crn, id]) => [id, crn]))
+
+export const person = [
+  {
+    method: 'GET',
+    path: '/v2/person/{personId}/summary',
+    handler: async (request, h) => {
+      const crn = idCrnCache[request.params.personId]
+
+      if (crn) {
+        return h.response({ _data: createPerson(parseInt(request.params.personId), crn) })
+      }
+
+      return Boom.notFound('Person not found')
+    }
+  },
+  {
+    method: 'POST',
+    path: '/v2/person/search',
+    handler: async (request, h) => {
+      const body = request.payload
+
+      if (!body?.searchFieldType || !body?.primarySearchPhrase) {
+        return Boom.badRequest('Invalid or missing search parameters')
+      }
+
+      let personId = crnIdCache[body.primarySearchPhrase]
+      if (!personId) {
+        return h.response({ _data: [], _page: pagination0 })
+      }
+
+      return h.response({
+        _data: [createPerson(personId, body.primarySearchPhrase)],
+        _page: pagination
+      })
+    }
+  }
+]
