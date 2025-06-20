9122825654c1106094ba95b3ea897c0500d56998
Robin Knipe <robin.knipe@defra.gov.uk>
2025-06-20 15:41:02 +0100

    test: fix test to reflect API responses

--- test/integration/routes/person/person.test.js
+++ test/integration/routes/person/person.test.js
@@ -10,28 +10,49 @@ describe('Fake Person', () => {
   })
 
   it('should fetch the same person with ID or CRN', async () => {
-    const id = 1000009
-    const firstPerson = (
-      await server.inject({
-        method: 'GET',
-        url: `/v2/person/${id}/summary`
-      })
-    ).result._data
-    expect(firstPerson.id).toBe(id)
+    const id = 11111111
+    const { result, statusCode } = await server.inject({
+      method: 'GET',
+      url: `/person/${id}/summary`
+    })
+    expect(statusCode).toBe(200)
+    expect(result._data.id).toBe(id)
+    const {
+      firstName,
+      lastName,
+      primaryAddress,
+      personalIdentifiers,
+      nationalInsuranceNumber,
+      customerReferenceNumber,
+      email,
+      locked,
+      deactivated
+    } = result._data
 
-    const samePerson = (
-      await server.inject({
-        headers: {
-          'content-type': 'application/json'
-        },
-        method: 'POST',
-        url: '/v2/person/search',
-        payload: {
-          searchFieldType: 'crn',
-          primarySearchPhrase: firstPerson.customerReferenceNumber
-        }
-      })
-    ).result._data
-    expect(samePerson).toEqual([firstPerson])
+    const res2 = await server.inject({
+      headers: {
+        'content-type': 'application/json'
+      },
+      method: 'POST',
+      url: '/person/search',
+      payload: {
+        searchFieldType: 'CUSTOMER_REFERENCE',
+        primarySearchPhrase: customerReferenceNumber
+      }
+    })
+    expect(res2.statusCode).toBe(200)
+    expect(res2.result._data.length).toBe(1)
+    const samePerson = res2.result._data[0]
+    expect(samePerson).toEqual({
+      id,
+      fullName: `${firstName} ${lastName}`,
+      primaryAddress: primaryAddress,
+      personalIdentifiers: personalIdentifiers,
+      nationalInsuranceNumber: nationalInsuranceNumber,
+      customerReference: customerReferenceNumber,
+      email: email,
+      locked: locked,
+      deactivated: deactivated
+    })
   })
 })
