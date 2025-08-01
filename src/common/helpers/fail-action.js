937f59a22fb15f54a341ceac60414ae7657c2f10
Robin Knipe <robin.knipe@defra.gov.uk>
2025-08-01 13:46:00 +0100

    feat: improve & extract error handling
    Better emulate the standard upstream error responses, but still capture
    potentially useful detail in the los.
    Move the error response handling out of the server code to reduce
    complexity and better separate concerns.

--- src/common/helpers/fail-action.js
+++ src/common/helpers/fail-action.js
@@ -6,3 +6,58 @@ export function failAction(_request, _h, error) {
   logger.warn(error, error?.message)
   throw error
 }
+
+export const emulateUpstreamErrors = (request, h) => {
+  const { headers, info, method, path, payload, response } = request
+
+  if (response.isBoom) {
+    const code = response.output.statusCode
+    logger.warn({
+      http: {
+        request: {
+          id: info.id,
+          method,
+          ...(headers && { headers })
+        },
+        response: {
+          status_code: code,
+          ...(info?.received && { response_time: new Date().getTime() - info.received })
+        }
+      },
+      error: {
+        code,
+        type: response.name,
+        message: response.message,
+        stack_trace: response.stack
+      },
+      message: `${method}${payload ? ' ' + JSON.stringify(payload) : ''} ${path} ${code}`
+    })
+
+    // Replace payload for client with standard upstream error responses
+    if (code === 400) {
+      return h.response({ code, message: 'HTTP 400 Bad Request' }).code(code)
+    }
+    if (code === 403) {
+      return h
+        .response(
+          '<html><body><h1>403 Forbidden</h1>\n' +
+            'Request forbidden by administrative rules.\n</body></html>\n'
+        )
+        .code(code)
+    }
+    if (code === 404) {
+      return h.response({ code, message: 'HTTP 404 Not Found' }).code(code)
+    }
+    if (code === 500) {
+      return h
+        .response({
+          code,
+          message: 'There was an error processing your request. It has been logged (ID someID)'
+        })
+        .code(code)
+    }
+    return h.response({ code, message: response.message }).code(code)
+  }
+
+  return h.continue
+}
