f78dd1ae7b617d13f4c686e8ec9b34d891f0fe71
Robin Knipe <robin.knipe@defra.gov.uk>
2025-08-11 10:05:04 +0100

    feat: add applications route to old endpoints
    ...so it can be used while we prepare to switch-over fully to the new
    faked mock endpoints.

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -5,6 +5,7 @@ import { organisation } from '../routes/rural-payments/organisation.js'
 import { lms } from '../routes/rural-payments/lms.js'
 import { notifications } from '../routes/rural-payments/messages.js'
 import { sitiAgri } from '../routes/rural-payments/siti-agri.js'
+import { sitiagri } from '../routes/v2/siti-agri.js'
 
 const router = {
   plugin: {
@@ -17,7 +18,8 @@ const router = {
         ...authenticate,
         ...lms,
         ...notifications,
-        ...sitiAgri
+        ...sitiAgri,
+        ...sitiagri
       ])
     }
   }
