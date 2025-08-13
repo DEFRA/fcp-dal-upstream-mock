b66563bd399d31676d02dc43dcaa6e3f5a07984d
Robin Knipe <robin.knipe@defra.gov.uk>
2025-08-13 18:40:04 +0100

    fix: correct v1 route for applications data

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,9 +1,9 @@
 import { health } from '../routes/health.js'
 import { authenticate } from '../routes/rural-payments/authenticate.js'
-import { person } from '../routes/rural-payments/person.js'
-import { organisation } from '../routes/rural-payments/organisation.js'
 import { lms } from '../routes/rural-payments/lms.js'
 import { notifications } from '../routes/rural-payments/messages.js'
+import { organisation } from '../routes/rural-payments/organisation.js'
+import { person } from '../routes/rural-payments/person.js'
 import { sitiAgri } from '../routes/rural-payments/siti-agri.js'
 import { sitiagri } from '../routes/v2/siti-agri.js'
 
@@ -19,7 +19,10 @@ const router = {
         ...lms,
         ...notifications,
         ...sitiAgri,
-        ...sitiagri
+        ...sitiagri.map((route) => ({
+          ...route,
+          path: `/v1${route.path}` // change path to v1
+        }))
       ])
     }
   }
