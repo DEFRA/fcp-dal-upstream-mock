56bf7c19d6682bff3d12ea63f4f872fe817bd12c
Robin Knipe <robin.knipe@defra.gov.uk>
2025-08-14 08:26:35 +0100

    test: fix upstream contract test mutations

--- test/contract/check-schema.sh
+++ test/contract/check-schema.sh
@@ -50,36 +50,29 @@ fi
 case "$1" in
   p | person)
     schema="person"
-    mutations='
-. |
-.schemes = "https" |
-.paths["/person/{personId}/summary"].get.parameters[0]["x-examples"] = [5858232,5108985,5108989] |
-.definitions.PartySearchRequest["x-examples"][0].primarySearchPhrase = "1105658066" |
-.definitions.PartySearchRequest["x-examples"][1].primarySearchPhrase = "1101089857" |
-.definitions.PartySearchRequest["x-examples"][2].primarySearchPhrase = "1101089899"'
+    mutations='. |
+.paths["/person/{personId}/summary"].get.parameters[0].schema.examples] = [5858232,5108985,5108989] |
+.components.schemas.PartySearchRequest["x-examples"][0].primarySearchPhrase = "1105658066" |
+.components.schemas.PartySearchRequest["x-examples"][1].primarySearchPhrase = "1101089857" |
+.components.schemas.PartySearchRequest["x-examples"][2].primarySearchPhrase = "1101089899"'
     ;;
   o | org | organisation)
     schema="organisation"
-    mutations='
-. |
-.schemes[0] = "https" |
-.paths["/organisation/{organisationId}"].get.parameters[0]["x-examples"] = [5491723,5680131,5849659,5852711,5858233 ] |
-.definitions.PartySearchRequest["x-examples"][0].primarySearchPhrase = "106554744" |
-.definitions.PartySearchRequest["x-examples"][1].primarySearchPhrase = "200629003" |
-.definitions.PartySearchRequest["x-examples"][2].primarySearchPhrase = "200665008"'
+    mutations='. |
+.paths["/organisation/{organisationId}"].get.parameters[0].schema.examples = [5491723,5680131,5849659,5852711,5858233 ] |
+.components.schemas.PartySearchRequest["x-examples"][0].primarySearchPhrase = "106554744" |
+.components.schemas.PartySearchRequest["x-examples"][1].primarySearchPhrase = "200629003" |
+.components.schemas.PartySearchRequest["x-examples"][2].primarySearchPhrase = "200665008"'
     ;;
   a | auth | authenticate )
     schema="authenticate"
-    mutations='
-. |
-.schemes[0] = "https" |
+    mutations='. |
 .paths["/external-auth/security-answers/{crn}"].get.parameters[0].pattern = "^[1-9][0-9]{9,19}$" |
-.paths["/external-auth/security-answers/{crn}"].get.parameters[0]["x-examples"] = [1105739979,1106046692,1106077237,1100932879,1105430162]'
+.paths["/external-auth/security-answers/{crn}"].get.parameters[0].schema.examples] = [1105739979,1106046692,1106077237,1100932879,1105430162]'
     ;;
   s | sa | siti-agri)
     schema="siti-agri"
-    mutations='
-. |
+    mutations='. |
 .paths["/SitiAgriApi/cv/appByBusiness/sbi/{sbi}/list"].get.parameters[0].schema.examples = [121174131,200697200,107120488,117713636,200694241,200721391,119897756]'
     ;;
   h | help | --help | -h)
